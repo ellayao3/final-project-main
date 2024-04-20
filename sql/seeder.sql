1b00da9c260c7e9fc18fdfe00abc1281250b3721
Daniella Yao <129978948+ellayao3@users.noreply.github.com>
2024-04-20 13:27:11 -0400

    moreUpdates

--- sql/seeder.sql
+++ sql/seeder.sql
@@ -13,9 +13,12 @@ VALUES ('Melanie Q.', 5.0, 'Let me start off by saying this is the BEST veterina
 INSERT INTO `reviews` (`name`, `star`, `review_message`)
 VALUES ('Xue W.', 5.0, 'I swear these people should have a straight ticket to heaven for what they do. I have a huge amount of respect for people that can stomach being able to take the best of care for something as helpless as an animal and these guys are the best. Everyone from the front desk to Dr. Yao were incredibly friendly and accommodating. They explained everything in detail and put the needs of my cat over anything else. Came in because my cat was limping, they were able to give me an estimate before I committed to treatment, to which I found pretty standard as far as elite veterinarians are concerned. Made an appointment the day before and had 0 wait time when coming in today. Will be using them from here on out and highly recommend to anyone looking for a vet in Miami.');
 
+INSERT INTO `reviews` (`name`, `star`, `review_message`)
+VALUES ('Cinthia T.', 4.0, 'Went To Yao Animal Hospital For The First Time After My Dog Had Been Vomiting And Having The Runs All Night, After A Trip From The Groomer The Day Before. I Was Terrified That He May Have Had A Virus Or Something Serious. I Happened To Call Yaos And Was Asked To Bring My Dog In ASAP. My Dog And I Were Very Pleased With The Service Received. They Were Very Attentive, Didnt Make Us Wait And Were Extremely Nice. I Would Recommend YAO Animal Hospital And Hope Everyone Has As A Great Experience As I Did.');
+
 
 -- create table for appointments --
-CREATE TABLE `appointments` (
+CREATE TABLE `addAppointments` (
     `id` INT AUTO_INCREMENT PRIMARY KEY,
     `name` VARCHAR(255) NOT NULL,
     `number` VARCHAR(20) NOT NULL,
