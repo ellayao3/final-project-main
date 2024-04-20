6033293d6319c0a62341553495cd9c508d8a624a
Daniella Yao <129978948+ellayao3@users.noreply.github.com>
2024-04-20 10:13:31 -0400

    add

--- public/index.php
+++ public/index.php
@@ -6,6 +6,8 @@ $env = parse_ini_file('../.exampleenv');
 require '../app/core/config.php';
 
 use app\core\Router;
+use app\controllers\MainController;
+use app\controllers\UserController;
 use app\controllers\AppointmentsController;
 use app\controllers\ReviewsController;
 
@@ -14,5 +16,6 @@ require_once __DIR__ . '/helpers.php';
 //to add a new route add to the app/core/routes.php array
 $router = new Router($routes);
 $router->serveRoute();
+
 ?>
 
