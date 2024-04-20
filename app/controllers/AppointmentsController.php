14d897cb7f67efff69b9cac11a18b27bcd496878
Daniella Yao <129978948+ellayao3@users.noreply.github.com>
2024-04-20 11:07:17 -0400

    ac update

--- app/controllers/AppointmentsController.php
+++ app/controllers/AppointmentsController.php
@@ -1,35 +1,16 @@
 <?php
+
 namespace app\controllers;
+use app\models\Appointments; 
 
-use app\core\Controller;
-use app\models\Appointments;
-
-class AppointmentsController extends Controller
+class AppointmentsController 
 {
-    protected $model;
+    public function getExperience() {
+        $appointmentsModel = new Experience();
+        header("Content-Type: application/json");
+        $appointments = $appointmentseModel->getAllAppointments(); 
+        echo json_encode($appointments);
+        exit();
+    }    
 
-    public function __construct()
-    {
-        $this->model = new Appointments(); 
-    }
-
-    public function postAppointments()
-    {
-        // Retrieve form data from the POST request
-        $name = $_POST['name'];
-        $number = $_POST['number'];
-        $email = $_POST['email'];
-        $date = $_POST['date'];
-
-        // Set the database connection for the model
-        $this->model->setDB($this->getDb());
-
-        // Insert form data into the database
-        $this->model->saveAppointments($name, $number, $email, $date);
-
-        // Inserting into the database is completed for appointments
-        // Now, you can insert the same data into the contact form table
-        $this->model->saveAppointmentsInfo($name, $email, $number, $date);
-    }
 }
-?>
